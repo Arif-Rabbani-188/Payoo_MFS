8261e99f4f86ae5af0c89c610f209081ce6b93c7
KHAN MAHMUD ARIF RABBANI <snigdho70@gmail.com>
2025-02-25 22:17:47 +0600

    amount correction done

---
+++ script/cash.js
@@ -0,0 +1,49 @@
+let addMoneyBtn = document.getElementById("addMoneyBtn");
+let mainBalance = document.getElementById("main-balance");
+let sum = parseInt(mainBalance.innerText);
+
+addMoneyBtn.addEventListener("click", function () {
+  let addMoneyAmount = document.getElementById("addMoneyAmount").value;
+  addMoneyAmount = parseInt(addMoneyAmount);
+
+  sum = sum + addMoneyAmount;
+
+  console.log(sum);
+
+  mainBalance.innerText = sum;
+});
+
+let cashOutBtn = document.getElementById("cashOutBtn");
+
+cashOutBtn.addEventListener("click", function () {
+  let cashOutAmount = document.getElementById("cashOutAmount").value;
+
+  cashOutAmount = parseInt(cashOutAmount);
+
+  console.log(cashOutAmount);
+
+  sum = sum - cashOutAmount;
+
+  mainBalance.innerText = sum;
+});
+
+
+let transferBtn = document.getElementById("transferBtn");
+
+
+transferBtn.addEventListener("click", function (){
+    let transferAmount = parseInt(document.getElementById("transferAmount").value);
+
+    sum = sum - transferAmount;
+
+    mainBalance.innerText = sum;
+})
+
+
+document.getElementById("billBtn").addEventListener("click", function (){
+    let billAmount = parseInt(document.getElementById("billAmount").value);
+
+    sum = sum - billAmount;
+
+    mainBalance.innerText = sum;
+})
